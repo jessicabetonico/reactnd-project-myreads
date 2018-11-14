2c9b162fa2247c4418ef7dba5d63a9dd5df4c297
jessicabetonico <jessicabetonico@gmail.com>
2018-11-13 23:29:35 -0200

    Altera Component MyReads para uma Stateless Functional Component

--- src/MyReads.js
+++ src/MyReads.js
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import BookShelf from './BookShelf';
 import PropTypes from 'prop-types';
 import './App.css';
 
-class MyReads extends Component {
-  static propTypes = {
-    shelfs: PropTypes.object.isRequired,
-    onUpdateShelfs: PropTypes.func.isRequired,
-    loading: PropTypes.bool.isRequired,
-  }
+const MyReads = ({shelfs, onUpdateShelfs, loading}) => {
+  const shelfsKeys = Object.keys(shelfs);
 
-  render() {
-    const {shelfs, onUpdateShelfs, loading} = this.props;
-    const shelfsKeys = Object.keys(shelfs);
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          {loading && <div className="loader-center"/>}
-          {shelfsKeys.map((key) => (
-            <BookShelf
-              key={key}
-              name={shelfs[key].name}
-              books={shelfs[key].books}
-              onUpdateShelfs={onUpdateShelfs}
-              loading={loading}
-            />
-          ))}
-        </div>
-
-        <Link
-          to='/search'
-          className="open-search"
-        >Add a book</Link>
-
+  return(
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
-}
+      <div className="list-books-content">
+        {loading && <div className="loader-center"/>}
+        {shelfsKeys.map((key) => (
+          <BookShelf
+            key={key}
+            name={shelfs[key].name}
+            books={shelfs[key].books}
+            onUpdateShelfs={onUpdateShelfs}
+            loading={loading}
+          />
+        ))}
+      </div>
+
+      <Link
+        to='/search'
+        className="open-search"
+      >Add a book</Link>
+    </div>    
+  )
+};
+
+MyReads.propTypes = {
+  shelfs: PropTypes.object.isRequired,
+  onUpdateShelfs: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 export default MyReads;
